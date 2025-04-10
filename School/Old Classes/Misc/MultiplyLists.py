a2e564c6eeaed96b147fcd843a04a762ce955362
Joshua Newberry <joshuamnewberry@gmail.com>
2025-04-09 21:55:44 -0400

    Delete School/Old Classes/Misc directory (duplicate)

--- School/Old Classes/Misc/MultiplyLists.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-# Cross multiply two lists and return it as a 2D list
-def multiplyList(a:List[int|float], b:List[int|float]) -> List[List[int|float]]:
-    lst:List[List[None]] = [[None for i in range(len(a))] for j in range(len(b))]
-    for y in range(len(b)):
-        for x in range(len(a)):
-            lst[y][x] = a[x] * b[y]
-    return lst
